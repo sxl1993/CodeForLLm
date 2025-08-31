baadb2063edd6dc6e5ded7672748fa8fc8346d2c
root <root@szzj-isa-ai-peking-poc13.szzj.baidu.com>
2025-08-31 23:31:56 +0800

    add 704

--- leetcode/704.py
+++ leetcode/704.py
@@ -3,31 +3,37 @@ from typing import List
 704. 二分查找
 """
 def search_v1(nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            middle = left + (right - left) // 2
-            if nums[middle] > target:
-                right = middle - 1
-            elif nums[middle] < target:
-                left = middle + 1
-            else:
-                return middle
-        return -1
+    """
+    [left, right]
+    """
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        middle = left + (right - left) // 2
+        if nums[middle] > target:
+            right = middle - 1
+        elif nums[middle] < target:
+            left = middle + 1
+        else:
+            return middle
+    return -1
     
 
 def search_v2(nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums)
-        while left < right:
-            middle = left + (right - left) // 2
-            if nums[middle] > target:
-                right = middle
-            elif nums[middle] < target:
-                left = middle + 1
-            else:
-                return middle
-        return -1  
+    """
+    [left, right)
+    """
+    left = 0
+    right = len(nums)
+    while left < right:
+        middle = left + (right - left) // 2
+        if nums[middle] > target:
+            right = middle
+        elif nums[middle] < target:
+            left = middle + 1
+        else:
+            return middle
+    return -1  
 
         
 if __name__ == '__main__':
